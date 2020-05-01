785cc9d9eeb1fa9585ac78920437bd49a33faa51
Fabio Dall'Antonia <fabio@VPN0171.desy.de>
2020-05-01 19:22:34 +0200

    Remove static bin folder Change entry level in cell_check.py

--- extra_xwiz/cell_check.py
+++ extra_xwiz/cell_check.py
@@ -1,10 +1,9 @@
 from argparse import ArgumentParser
-import sys
 from extra_xwiz.utilities import (get_crystal_frames, fit_unit_cell,
                                   replace_cell) 
 
 
-def main(stream_file, cell_file, out_cell=False):
+def check_cells(stream_file, cell_file, out_cell=False):
     print(f'Checking {stream_file} against {cell_file}')
     hit_list, cell_ensemble = get_crystal_frames(stream_file, cell_file)
     with open('xtal_checker_hits.lst', 'w') as f:
@@ -15,11 +14,11 @@ def main(stream_file, cell_file, out_cell=False):
         replace_cell(cell_file, refined_cell)
 
 
-if __name__ == '__main__':
+def main(argv=None):
     ap = ArgumentParser(prog="xwiz-cell-checker")
     ap.add_argument('stream_file', help='stream file to parse')
     ap.add_argument('cell_file', help='reference cell file for filtering')
     ap.add_argument('-o', '--out_cell', help='optional cell file to write',
                     action='store_true')
-    args = ap.parse_args(sys.argv[1:])
-    main(args.stream_file, args.cell_file, out_cell=args.out_cell)
+    args = ap.parse_args(argv)
+    check_cells(args.stream_file, args.cell_file, out_cell=args.out_cell)
